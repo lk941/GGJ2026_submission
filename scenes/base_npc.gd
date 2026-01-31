12b0dea37322c5eb5b9a708133444e1a8a8b2390
lk941 <leekee941@gmail.com>
2026-02-01 01:07:13 +0800

    integrated art

--- scenes/base_npc.gd
+++ scenes/base_npc.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var dialogue_id := "sheep"
+@export var dialogue_id := "test"
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
@@ -8,14 +8,16 @@ extends CharacterBody2D
 func _ready():
 	_animated_sprite.play()
 
-
-func _on_mouse_entered():
-	
-	print("hello")
+#
+#func _on_mouse_entered():
+	#dialogue_id = get_parent().name
+	#
+	#print(dialogue_id)
 
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int):
 	if event is InputEventMouseButton and event.pressed:
+		dialogue_id = get_parent().name
 		print("Character clicked!")
 		var ui = get_tree().get_first_node_in_group("dialogue_ui")
 		ui.show_dialogue(DialogueData.dialogues[dialogue_id])
